4978fb0277b15e860540cdbf115b84e9456b0d7d
Alberto Luz <albertoluz2001@gmail.com>
2017-06-21 13:27:26 -0700

    add javascript files

---
+++ Javascriptbasics/0.05_conditionals.js
@@ -0,0 +1,38 @@
+
+//if & else
+
+
+var age = 40;
+
+if(age === 40){
+    console.log("Man, you are so cool");
+} else if (age >= 25){
+	console.log("You can rent a car");
+} else if(age >= 18){
+	console.log("congrats you can vote");
+}
+
+if(age === 18){
+	console.log("Get a job!!!");
+}
+
+
+
+
+var password = "99Davids";
+
+if(password === "99Davids"){
+	console.log("Welcome Alberto");
+} else {
+	console.log("Who are you")
+}
+
+
+//Write a conditional statement for some 
+//creating a new password
+
+//You can't use your old pw
+//Needs to be 8 characters 
+//Must have a digit
+//Must have a speacial symbol 
+
